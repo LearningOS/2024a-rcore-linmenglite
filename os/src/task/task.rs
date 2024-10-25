3455fb54b4d5630428cf4b62831344597d0af6a3
linmenglite <1017345395@qq.com>
2024-10-25 13:57:17 +0800

    update

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,25 +1,27 @@
 //! Types related to task management
 
 use super::TaskContext;
-
-/// The task control block (TCB) of a task.
+use crate::config::MAX_SYSCALL_NUM;
+// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
 pub struct TaskControlBlock {
-    /// The task status in it's lifecycle
+    // The task status in it's lifecycle
     pub task_status: TaskStatus,
-    /// The task context
+    // The task context
     pub task_cx: TaskContext,
+    pub start_time: usize,
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
 }
 
-/// The status of a task
+// The status of a task
 #[derive(Copy, Clone, PartialEq)]
 pub enum TaskStatus {
-    /// uninitialized
+    // uninitialized
     UnInit,
-    /// ready to run
+    // ready to run
     Ready,
-    /// running
+    // running
     Running,
-    /// exited
+    // exited
     Exited,
 }
